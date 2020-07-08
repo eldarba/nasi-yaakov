76682f45f447cd5f3eb12b8c4e64584e5eb37617
jbt <jbt@54914_>
2020-07-08 11:04:09 +0300

    aaa

---
+++ Lesson 01 intro/src/b/Tar1.java
@@ -0,0 +1,17 @@
+package b;
+
+public class Tar1 {
+
+	public static void main(String[] args) {
+
+		int x = 5;
+		float y = 20.5F;
+		long z = 1500;
+
+		float sum = x + y + z;
+		float avg = sum / 3;
+		System.out.println(sum);
+		System.out.println(avg);
+
+	}
+}
---
+++ Lesson 01 intro/src/b/Tar2.java
@@ -0,0 +1,15 @@
+package b;
+
+public class Tar2 {
+
+	public static void main(String[] args) {
+
+		float f = 100_000F;
+		System.out.println(f);
+
+		f += 0.001;
+		System.out.println(f);
+
+	}
+
+}
